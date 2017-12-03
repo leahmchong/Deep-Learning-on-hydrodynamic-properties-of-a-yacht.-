d62a3142b131d174ba0f1748c0b1aa501e2ee993
Suyash Nigam <33047254+suyash622@users.noreply.github.com>
2017-12-02 21:11:50 -0500

    Add files via upload

--- marine_data_LR.m
+++ marine_data_LR.m
@@ -12,7 +12,7 @@ x6=marine_data(1:250,6);
 dataA=marine_data;
 
 
-p = 1.0;     % proportion of rows to select for training
+p = 0.5;     % proportion of rows to select for training
 
 N = size(dataA,1)  % total number of rows 
 tf = false(N,1)    % create logical index vector
@@ -31,12 +31,15 @@ mdl = fitlm(X,y)
 A=table2array(mdl.Coefficients);
 B=A(1:7,1);
 Y=dataTesting(:,7);
-X_pred=B(1)+B(2:6)*X_test;
-mse_test=immse(X_pred,Y_test);
+[m,n]=size(dataTesting);
+bias=ones(m);
+bias=bias*A(1);
+X_pred=bias+X_test*B(2:7);
+mse_test=immse(X_pred(:,1),Y_test);
 scatter3(X(:,1),X(:,2),y)
 zlabel('Y');
 
-
+sqrt(mse_test)
 
 
 
